6b19e94eb1d91b8ce8f5fe5ca33cb31d9159e0e7
Shivacsehit <shivacsehit@gmail.com>
2026-03-22 20:14:32 +0530

    AppDbContextFactory added

---
+++ TinyUrl.API/Data/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TinyUrl.API.Data
+{
+    public class AppDbContextFactory
+        : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(
+                "Server=localhost\\SQLEXPRESS;Database=TinyUrlDb;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
